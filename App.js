92c59dbb253823da335435005ea078173f83e952
Matt Roberts <matt.roberts@getstream.io>
2021-11-04 13:22:48 -0600

    Start of support app

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
+import { AppRegistry } from 'react-native';
+import { StreamChat } from 'stream-chat';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+// import {APP_KEY} from "@env";
+
 export default function App() {
+const api_key = process.env['APP_KEY'];
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Matts super cool app</Text>
       <StatusBar style="auto" />
     </View>
   );
